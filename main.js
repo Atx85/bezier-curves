95bb01a09e4aad1f79561f6d5d99df41e9a54288
Attila <atx@noreply.com>
2020-10-02 22:54:28 +0100

    it works with n-1

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ let down = false
 let selected = false
 
 function getPc(p,t) {
-    // console.log('x1:',Math.pow((1-t),3) )
     let x = Math.pow((1-t),3)* p[0].vertices[0].x + 3*t*Math.pow((1-t),2)* p[1].vertices[0].x + 3*Math.pow(t,2)*(1-t)*p[2].vertices[0].x + Math.pow(t,3)*p[3].vertices[0].x
     let y = Math.pow((1-t),3)* p[0].vertices[0].y + 3*t*Math.pow((1-t),2)* p[1].vertices[0].y + 3*Math.pow(t,2)*(1-t)*p[2].vertices[0].y + Math.pow(t,3)*p[3].vertices[0].y
     return new Point(ctx,[[x,y]])
@@ -79,16 +78,12 @@ function update() {
         for(let i = 0; i < n; i++) {
             b[i] = nCi(n - 1, i) * (Math.pow(T,i)) * (Math.pow((1-T), (n - 1 - i)) )
             x += P[i].vertices[0].x * b[i]
-            // if(i === 0) console.log('xx1',b[i], n-i)
             y += P[i].vertices[0].y * b[i]
         }
 
         let c = new Point(ctx, [[x, y]])
-        // c = getPc(P,T)
-        // console.log(c.vertices[0].x,c.vertices[0].y, x,y )
         c.draw()
     }
-    // console.log(C)
 }
 
 
